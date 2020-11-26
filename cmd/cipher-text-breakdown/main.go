eaeb010decb35f8ea51cda6c6325fa53299ac4b9
Doug Saylor <doug@saylorsolutions.com>
2020-11-26 17:14:27 -0600

    Clarifying variable meaning

--- cmd/cipher-text-breakdown/main.go
+++ cmd/cipher-text-breakdown/main.go
@@ -18,13 +18,13 @@ func main() {
 		panic("An error occurred encrypting data!")
 	}
 
-	lenCipherText := len(cipherText)
-	fmt.Printf("Length of encrypted data: %d\n", lenCipherText)
+	totalCipherTextLen := len(cipherText)
+	fmt.Printf("Length of encrypted data: %d\n", totalCipherTextLen)
 	fmt.Printf("Actual cipher text length plus authentication tag is total length - GCM nonce length - scrypt salt length\n")
-	cipherTextLen := lenCipherText - gcmNonceLen - scryptSaltLen
-	fmt.Printf("\t%d - %d - %d = %d\n", lenCipherText, gcmNonceLen, scryptSaltLen, cipherTextLen)
+	actualCipherTextLen := totalCipherTextLen - gcmNonceLen - scryptSaltLen
+	fmt.Printf("\t%d - %d - %d = %d\n", totalCipherTextLen, gcmNonceLen, scryptSaltLen, actualCipherTextLen)
 	fmt.Printf("Removing the authentication tag length (%d) from the cipher text length yields the same size as the input\n", authenticationTagLen)
-	fmt.Printf("\t%d - %d = %d\n", cipherTextLen, authenticationTagLen, cipherTextLen-authenticationTagLen)
+	fmt.Printf("\t%d - %d = %d\n", actualCipherTextLen, authenticationTagLen, actualCipherTextLen-authenticationTagLen)
 	fmt.Printf("\tlen('%s') = %d\n\n", string(secretData), len(secretData))
 
 	fmt.Printf("This means that for any input size of n bytes, the encrypted size will be n + %d + %d + %d bytes\n", gcmNonceLen, scryptSaltLen, authenticationTagLen)
